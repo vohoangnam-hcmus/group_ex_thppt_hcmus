f7c6eff4024fea86de7f780a0a69a22deb843eda
Hoang_Nam <Hoang_Nam@Hoang-Nam-PC>
2023-12-15 19:53:00 +0700

    Delete file

--- NewtonInterpolation.m
+++
@@ -1,4 +0,0 @@
-function result = NewtonInterpolation(xa, ya, x)
-    da = DividedDifference(xa, ya);
-    result = NewtonForm(xa, da, x);
-end
--- tichphansimpson13.m
+++
@@ -1,7 +0,0 @@
-function KQ = tichphansimpson13(fx, a, b, N) 
-    fx = str2func(['@(x)',fx]);
-    h = (b - a) / N;
-    x = a + (0:N) * h;
-    fx = feval(fx, x); 
-    KQ = h/3 * (fx(1) + 4*sum(fx(2:2:N)) + 2*sum(fx(3:2:N-1)) + fx(N+1));
-end 
